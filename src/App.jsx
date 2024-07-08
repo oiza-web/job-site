966159e37e0804613595f2c7adb5bceb4ff73dd4
oiza-web <maryolorunfemi6@gmail.com>
2024-07-08 17:26:07 +0100

    Refactored design, divided it into components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import HeroComponent from "./components/HeroComponent";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 const App = () => {
   return (
     <>
@@ -9,15 +10,7 @@ const App = () => {
       <HeroComponent />
       <HomeCards />
       <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJobs />
     </>
   );
 };
